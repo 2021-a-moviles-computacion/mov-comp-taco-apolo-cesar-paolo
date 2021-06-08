51e3e2b0f4c9c847eb393702b81812a277214606
Cesar Taco <cesar.taco@epn.edu.ec>
2021-06-08 13:04:02 -0500

    Add project files created by default

---
+++ 00-kotlin/src/main/kotlin/Main.kt
@@ -0,0 +1,30 @@
+import java.util.*
+
+fun main(){
+    println("Hola Mundo")
+    // tipo nombre = valor
+    // int edad = 12;
+    var edadProfesor: Int = 32
+    // var edadProfesor: Int = 32
+    var sueldoProfesor = 1.23
+    edadProfesor = 23
+    sueldoProfesor = 2.33
+    //Duck Typing
+
+    // Mutables / Inmutables
+    // Mutables (Re asignar "=")
+    var edadCachorro = 2
+    edadCachorro = 13
+    edadCachorro = 12
+
+    // Inmutables (NO Re asignar "=")
+    val numeroCedula = 1722488671
+    //numeroCedula = 17254465
+
+    //TIPOS DE VARIABLES (JAVA) INT, BOOLEAN, DOUBLE, FLOAT, CHAR
+    val nombreProfesor1: String = "César Taco"
+    val sueldo: Double = 1.23
+    val estadoCivil: Char = 'S'
+    val fechaNacimiento: Date = Date()
+
+}

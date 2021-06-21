134478ea312653b10d5612c91dfcf2a14ec5411b
Cesar Taco <cesar.taco@epn.edu.ec>
2021-06-21 13:01:02 -0500

    Añadir actividad ciclo de vida (default files)

--- 01-android/app/src/main/java/com/example/moviles_computacion_2021_b/MainActivity.kt
+++ 01-android/app/src/main/java/com/example/moviles_computacion_2021_b/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.example.moviles_computacion_2021_b
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnIrCicloVida = findViewById<Button>(
+            R.id.btn_ciclo_vida
+        )
+
+        btnIrCicloVida
+            .setOnClickListener {
+                abrirCicloVida()
+            }
+    }
+
+    fun abrirCicloVida(){
+        val intentExplicito = Intent(
+            this,
+            ACicloDeVida::class.java
+        )
+        startActivity(intentExplicito)
     }
 }

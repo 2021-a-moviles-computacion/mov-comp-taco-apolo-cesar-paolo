693d3da40e020ad526fbaa29e11034819fb5cc09
Cesar Taco <cesar.taco@epn.edu.ec>
2021-07-01 20:45:19 -0500

    add methods in Equipo class

--- 02-Deber01/src/main/kotlin/classes/Equipo.kt
+++ 02-Deber01/src/main/kotlin/classes/Equipo.kt
@@ -1,4 +1,50 @@
 package classes
 
+import java.util.*
+import kotlin.collections.ArrayList
+
 class Equipo {
+    val nombre : String
+    val titulos : Int?
+    val fechaFundacion : Date
+    val estado : Boolean //True -> activo | False -> inactivo
+    val presupuesto : Double?
+
+    init {
+        println("Inicializando Equipo....")
+        nombre = readLine().toString()
+        titulos = readLine()?.toInt()
+        fechaFundacion = Date()
+        estado = readLine().toBoolean()
+        presupuesto = readLine()?.toDouble()
+    }
+
+    fun ingresarFecha(){
+        val date : Date
+
+    }
+
+    fun crearEquipo(equipo: Equipo){
+        val crearEquipo = ArrayList<Equipo>()
+        crearEquipo.add(equipo)
+        println(equipo.toString())
+    }
+
+    fun leerEquipo(){
+        val leerEquipo = ArrayList<Equipo>()
+
+
+    }
+
+    fun actualizarEquipo(nombreEquipo: String){
+        val actualizarEquipo = ArrayList<Equipo>()
+
+
+    }
+
+    fun eliminarEquipo(){
+        val elminarEquipo = ArrayList<Equipo>()
+
+
+    }
 }

11cbb86c14c11d67cde6c9da88d0f41caaabd12b
Cesar Taco <cesar.taco@epn.edu.ec>
2021-08-17 13:00:00 -0500

    Login with firebase AuthUI

--- 06-firebase/app/src/main/java/com/example/firebase_uno/MainActivity.kt
+++ 06-firebase/app/src/main/java/com/example/firebase_uno/MainActivity.kt
@@ -1,11 +1,71 @@
 package com.example.firebase_uno
 
+import android.app.Activity
+import android.content.ActivityNotFoundException
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
 
 class MainActivity : AppCompatActivity() {
+
+    val CODIGO_INICIO_SESION = 102
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnLogin = findViewById<Button>(R.id.btn_login)
+
+        btnLogin.setOnClickListener {
+            llamarLoginUsuario()
+        }
     }
+
+    fun llamarLoginUsuario(){
+        val providers = arrayListOf(
+            //lista de proveedores
+            AuthUI.IdpConfig.EmailBuilder().build()
+        )
+
+        startActivityForResult(
+            AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .setTosAndPrivacyPolicyUrls(
+                    "https://example.com/terms.html",
+                "https://example.com/privacy.html"
+                )
+                .build(),
+            CODIGO_INICIO_SESION
+        )
+    }
+
+    override fun onActivityResult(
+        requestCode: Int,
+        resultCode: Int,
+        data: Intent?) {
+
+        super.onActivityResult(requestCode, resultCode, data)
+
+        when(requestCode){
+            CODIGO_INICIO_SESION -> {
+                if (resultCode == Activity.RESULT_OK){
+                    val usuario = IdpResponse.fromResultIntent(data)
+                    if (usuario?.isNewUser == true){
+                        Log.i("firebase-login","Nuevo Usuario")
+                    }else{
+                        Log.i("firebase-login","Antiguo Usuario")
+                    }
+                }else{
+                    Log.i("firebase-login","El usuario cancelo")
+                }
+            }
+        }
+
+    }
+
 }

f2e8836fcd1ce558ee242f3a23a3be161096b9b4
Cesar Taco <cesar.taco@epn.edu.ec>
2021-09-29 00:42:36 -0500

    project final version

--- 04-Proyecto/app/src/main/java/com/example/spotify_v1/Cancion.kt
+++
@@ -1,7 +0,0 @@
-package com.example.spotify_v1
-
-import android.media.Image
-                    //IMAGEN DEBER SER IMAGEN ALBUM
-class Cancion (val titulo: String, val imagen: String ){
-
-}
--- 04-Proyecto/app/src/main/java/com/example/spotify_v1/data/other/Constants.kt
+++
@@ -1,5 +0,0 @@
-package com.example.spotify_v1.data.other
-
-object Constants {
-    const val SONG_COLLECTION = "songs"
-}
